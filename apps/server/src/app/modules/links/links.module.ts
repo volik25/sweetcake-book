573d38ded6d0904c87a422ba9f1fd5f699ea96c4
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2022-11-20 17:26:27 +0300

    fix

--- apps/server/src/app/modules/links/links.module.ts
+++ apps/server/src/app/modules/links/links.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FilesModule } from '../files/files.module';
+import { FilesService } from '../files/files.service';
 import { LinksController } from './controllers/links.controller';
 import { LinksService } from './services/links.service';
 
 @Module({
   imports: [FilesModule],
-  providers: [LinksService],
+  providers: [LinksService, FilesService],
   controllers: [LinksController],
 })
 export class LinksModule {}
--- apps/server/src/app/modules/static/static.module.ts
+++ apps/server/src/app/modules/static/static.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { StaticService } from './services/static.service';
 import { StaticController } from './controllers/static.controller';
 import { FilesModule } from '../files/files.module';
+import { FilesService } from '../files/files.service';
 
 @Module({
   imports: [FilesModule],
-  providers: [StaticService],
+  providers: [StaticService, FilesService],
   controllers: [StaticController],
 })
 export class StaticModule {}

1a56ac198ddc31e8060d9b5c6c4307743c5ba6ae
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2022-11-20 16:22:27 +0300

    добавил все страницы

--- apps/server/src/app/app.controller.ts
+++ apps/server/src/app/app.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Param, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get()
+  @Get('/')
   @Render('index')
   home() {
+    return {};
+  }
+  @Get('/admin')
+  @Render('admin')
+  admin() {
+    return {};
+  }
+
+  @Get('/category/:id')
+  @Render('category/[id]')
+  category(@Param('id') id: string) {
     return {};
   }
 }

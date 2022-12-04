08ac1af2b1a4f6c9513a41f161f98ebdcfad91f2
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2022-12-04 14:36:08 +0300

    скрол ссобщений

--- apps/client-next/components/chat/Chat.tsx
+++ apps/client-next/components/chat/Chat.tsx
@@ -20,11 +20,16 @@ export const Chat = (): ReactElement => {
     };
   }, [socket]);
 
+  useEffect(() => {
+    messagesRef.current?.scrollTo(0, messagesRef.current.scrollHeight);
+  }, [messages]);
+
   socket.on('messageInput', (message) => {
     setMessages([...messages, { id: Date.now(), message, isMy: false }]);
   });
 
   const inputRef = useRef<HTMLInputElement>(null);
+  const messagesRef = useRef<HTMLDivElement>(null);
 
   const onMessageSend = () => {
     if (!inputRef.current) {
@@ -36,6 +41,7 @@ export const Chat = (): ReactElement => {
       { id: Date.now(), message: inputRef.current.value, isMy: true },
     ]);
     socket.emit('message', inputRef.current.value);
+    inputRef.current.value = '';
   };
   return (
     <div className={styles.chat}>
@@ -43,7 +49,10 @@ export const Chat = (): ReactElement => {
         <div>Консультация</div>
         <XCircle />
       </div>
-      <div className={cn(styles.messages, 'border-top border-bottom')}>
+      <div
+        className={cn(styles.messages, 'border-top border-bottom')}
+        ref={messagesRef}
+      >
         {messages.map((message) => (
           <div
             key={message.id}
@@ -56,7 +65,12 @@ export const Chat = (): ReactElement => {
         ))}
       </div>
       <div className={styles.chat__footer}>
-        <input type="text" className="form-control" ref={inputRef} />
+        <input
+          type="text"
+          className="form-control"
+          ref={inputRef}
+          onKeyUp={(event) => event.key == 'Enter' && onMessageSend()}
+        />
         <button className="btn btn-link" onClick={onMessageSend}>
           <SendFill />
         </button>
